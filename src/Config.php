8e52234deabeceb4d4675b1ed739402d6864ea6c
Mark Perkins <mark@allmarkedup.com>
2014-10-31 14:58:13 +0000

    Initial commit

---
+++ src/Config.php
@@ -0,0 +1,9 @@
+<?php namespace Amu\Conph;
+
+class Config
+{
+    public function __construct()
+    {
+        
+    }
+}
---
+++ src/Helper.php
@@ -0,0 +1,9 @@
+<?php namespace Amu\Conph;
+
+class Helper
+{
+    public function __construct()
+    {
+        
+    }
+}
